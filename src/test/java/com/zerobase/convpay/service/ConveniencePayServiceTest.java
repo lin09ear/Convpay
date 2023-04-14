e992c39247c4550c7b0f3b9ac0c9262404795f09
rui27sui <ruihwangruih@gmail.com>
2023-04-14 16:47:28 +0900

    심플 스프링 프레임 워크

--- src/test/java/com/zerobase/convpay/service/ConveniencePayServiceTest.java
+++ src/test/java/com/zerobase/convpay/service/ConveniencePayServiceTest.java
@@ -9,10 +9,18 @@ import com.zerobase.convpay.type.PayMethodType;
 import com.zerobase.convpay.type.PayResult;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ConveniencePayServiceTest {
-    ConveniencePayService conveniencePayService = new ConveniencePayService();
+    ConveniencePayService conveniencePayService = new ConveniencePayService(
+            new HashSet<>(
+                    Arrays.asList(new MoneyAdapter(), new CardAdapter())
+            ),
+            new DiscountByConvenience()
+    );
 
     @Test
     void pay_success() {
